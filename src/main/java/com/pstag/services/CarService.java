3088d1afd0dfe7233f6542a60d7c6fccb89ffc7b
Freddy <fredhopelane@gmail.com>
2024-12-27 17:29:47 +0700

    feat: Add Azure Blob Storage dependency and configure connection string in application properties; implement tests for image retrieval endpoints

--- src/main/java/com/pstag/services/CarService.java
+++ src/main/java/com/pstag/services/CarService.java
@@ -1,6 +1,9 @@
 package com.pstag.services;
 
+import org.eclipse.microprofile.config.ConfigProvider;
+
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.stream.Collectors;
 import java.util.List;
@@ -22,7 +25,11 @@ import io.vertx.mutiny.pgclient.PgPool;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import reactor.core.publisher.Mono;
 
+import com.azure.core.util.BinaryData;
+import com.azure.storage.blob.BlobServiceAsyncClient;
+import com.azure.storage.blob.models.BlockBlobItem;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -41,21 +48,31 @@ public class CarService {
 
     private final MyAiService aiService;
 
+    private final BlobServiceAsyncClient blobServiceAsyncClient;
+
+    private final String connectionString;
+
     @Inject
-    public CarService(MyAiService aiService) {
+    public CarService(MyAiService aiService, BlobServiceAsyncClient blobServiceAsyncClient) {
         this.aiService = aiService;
+        this.blobServiceAsyncClient = blobServiceAsyncClient;
+
+        connectionString = ConfigProvider.getConfig().getValue("quarkus.azure.storage.blob.connection-string",
+                String.class);
     }
 
     /**
-     * Retrieves a paginated list of CarEntity objects based on the provided filters, search criteria, and sorting options.
+     * Retrieves a paginated list of CarEntity objects based on the provided
+     * filters, search criteria, and sorting options.
      *
-     * @param client the PgPool client used to interact with the database
+     * @param client  the PgPool client used to interact with the database
      * @param filters a map of filter criteria to apply to the query
-     * @param search a search string to filter the results
-     * @param sorts a map of sorting options to apply to the query
-     * @param limit the maximum number of results to return
-     * @param offset the starting point in the list of results
-     * @return a Uni containing a TotalRowsAndData object with the total number of rows and the list of CarEntity objects
+     * @param search  a search string to filter the results
+     * @param sorts   a map of sorting options to apply to the query
+     * @param limit   the maximum number of results to return
+     * @param offset  the starting point in the list of results
+     * @return a Uni containing a TotalRowsAndData object with the total number of
+     *         rows and the list of CarEntity objects
      */
     public Uni<TotalRowsAndData<CarEntity>> findAll(PgPool client, Map<String, String> filters, String search,
             Map<String, String> sorts,
@@ -65,13 +82,14 @@ public class CarService {
     }
 
     /**
-     * Retrieves a list of CarEntity objects from the database based on the provided filters, sorts, and search criteria,
+     * Retrieves a list of CarEntity objects from the database based on the provided
+     * filters, sorts, and search criteria,
      * and converts the list to an XML string.
      *
-     * @param client the PgPool client used to interact with the database
+     * @param client  the PgPool client used to interact with the database
      * @param filters a map of filters to apply to the query
-     * @param sorts a map of sorting options to apply to the query
-     * @param search a search string to filter the results
+     * @param sorts   a map of sorting options to apply to the query
+     * @param search  a search string to filter the results
      * @return an XML string representation of the list of CarEntity objects
      */
     public String getXml(PgPool client, Map<String, String> filters, Map<String, String> sorts, String search) {
@@ -83,22 +101,25 @@ public class CarService {
     }
 
     /**
-     * Fills missing data for cars by fetching car information from an external AI service and updating the database.
+     * Fills missing data for cars by fetching car information from an external AI
+     * service and updating the database.
      *
      * @param client the PgPool client used for database operations
      * @return a GenericResponse indicating the success of the operation
      *
-     * This method performs the following steps:
-     * 1. Initializes filters and pagination parameters.
-     * 2. Iterates through pages of car data.
-     * 3. Fetches car data from the repository based on filters and pagination.
-     * 4. Concatenates car data into a single string.
-     * 5. Sends the concatenated data to an AI service to get additional car information.
-     * 6. Parses the response from the AI service.
-     * 7. Updates the car information in the database.
-     * 8. Logs the results of the operations.
+     *         This method performs the following steps:
+     *         1. Initializes filters and pagination parameters.
+     *         2. Iterates through pages of car data.
+     *         3. Fetches car data from the repository based on filters and
+     *         pagination.
+     *         4. Concatenates car data into a single string.
+     *         5. Sends the concatenated data to an AI service to get additional car
+     *         information.
+     *         6. Parses the response from the AI service.
+     *         7. Updates the car information in the database.
+     *         8. Logs the results of the operations.
      *
-     * If any JSON processing errors occur, they are logged.
+     *         If any JSON processing errors occur, they are logged.
      */
     public GenericResponse<String> fillMissingData(PgPool client) {
 
@@ -126,7 +147,7 @@ public class CarService {
             if (concatenatedData.isEmpty()) {
                 Log.info("No missing data found");
                 continue;
-            }   
+            }
 
             String response = aiService.getCarInformation(concatenatedData);
 
@@ -160,10 +181,12 @@ public class CarService {
 
     /**
      * Fetches UI parameters from the database using the provided PgPool client.
-     * The parameters include makers, makers and models, transmissions, colors, fuel types, and body types.
+     * The parameters include makers, makers and models, transmissions, colors, fuel
+     * types, and body types.
      * 
      * @param client the PgPool client used to interact with the database
-     * @return a Uni containing a GenericResponse with a map of UI parameters and a success message
+     * @return a Uni containing a GenericResponse with a map of UI parameters and a
+     *         success message
      */
     public Uni<GenericResponse<Map<String, Object>>> getUiParams(PgPool client) {
         Map<String, Uni<?>> uniMap = new HashMap<>();
@@ -178,7 +201,7 @@ public class CarService {
 
         // Combine all Uris into a single Uni
         return Uni.combine().all().unis(uniMap.values()).collectFailures()
-                .combinedWith(results -> {
+                .with(results -> {
                     Map<String, Object> response = new HashMap<>();
                     int i = 0;
                     for (String key : uniMap.keySet()) {
@@ -191,13 +214,17 @@ public class CarService {
     /**
      * Fetches the image URLs for a car based on its ID.
      *
-     * This method retrieves a car entity from the database using the provided client and ID.
-     * If the car is found, it checks for existing image URLs. If URLs are present, it returns them.
-     * If no URLs are found, it generates image URLs based on the car's details and updates the database.
+     * This method retrieves a car entity from the database using the provided
+     * client and ID.
+     * If the car is found, it checks for existing image URLs. If URLs are present,
+     * it returns them.
+     * If no URLs are found, it generates image URLs based on the car's details and
+     * updates the database.
      *
      * @param client the database client used to fetch the car entity
-     * @param id the ID of the car to fetch the image URLs for
-     * @return a Uni containing a GenericResponse with a map of color to image URL and a status message
+     * @param id     the ID of the car to fetch the image URLs for
+     * @return a Uni containing a GenericResponse with a map of color to image URL
+     *         and a status message
      */
     public Uni<GenericResponse<Map<String, String>>> getImage(PgPool client, Long id) {
         Uni<CarEntity> carUni = CarRepository.getById(client, id);
@@ -205,45 +232,63 @@ public class CarService {
             if (car == null) {
                 return Uni.createFrom().item(new GenericResponse<>(null, "Car not found"));
             }
-            List<String> colors = car.color();
+            List<String> colors = getColors(car);
             List<String> urls = car.imageUrl();
 
-            if (colors == null || colors.isEmpty()) {
-                colors = List.of("Black", "White", "Gray", "Silver", "Blue", "Red");
-            }
-
             if (urls != null && !urls.isEmpty()) {
-                Map<String, String> result = new HashMap<>();
-                for (int i = 0; i < colors.size(); i++) {
-                    result.put(colors.get(i), urls.get(i));
-                }
-                Log.info("Image fetched from database");
-                return Uni.createFrom().item(new GenericResponse<>(result, "Image fetched successfully"));
+                return Uni.createFrom().item(getResponseFromUrls(colors, urls));
             }
 
-            List<Uni<Map.Entry<String, String>>> imageUnis = colors.stream()
-                    .map(color -> {
-                        String keyword = "Car " + car.trimYear() + " " + car.make() + " " + car.model() + " " + car.trimName() + " " + color;
-                        return fetchImageUrl(keyword)
-                                .onItem().transform(url -> Map.entry(color, url));
-                    })
-                    .toList();
-
-            return Uni.combine().all().unis(imageUnis)
-                    .combinedWith(entries -> {
-                        Map<String, String> result = new HashMap<>();
-                        for (Object entry : entries) {
-                            Map.Entry<String, String> mapEntry = (Map.Entry<String, String>) entry;
-                            result.put(mapEntry.getKey(), mapEntry.getValue());
-                        }
-
-                        List<String> colorList = new ArrayList<>(result.keySet());
-                        List<String> urlList = new ArrayList<>(result.values());
-                        CarRepository.updateCarColorAndImageUrl(client, id, colorList, urlList);
-
-                        return new GenericResponse<>(result, "Image fetched successfully");
-                    });
+            return fetchAndStoreImageUrls(client, id, car, colors);
         });
+    }
+
+    private List<String> getColors(CarEntity car) {
+        List<String> colors = car.color();
+        if (colors == null || colors.isEmpty()) {
+            colors = List.of("Black", "White", "Gray", "Silver", "Blue", "Red");
+        }
+        return colors;
+    }
+
+    private GenericResponse<Map<String, String>> getResponseFromUrls(List<String> colors, List<String> urls) {
+        Map<String, String> result = new HashMap<>();
+        for (int i = 0; i < colors.size(); i++) {
+            result.put(colors.get(i), urls.get(i));
+        }
+        Log.info("Image fetched from database");
+        return new GenericResponse<>(result, "Image fetched successfully");
+    }
+
+    private Uni<GenericResponse<Map<String, String>>> fetchAndStoreImageUrls(PgPool client, Long id, CarEntity car,
+            List<String> colors) {
+        List<Uni<Map.Entry<String, String>>> imageUnis = colors.stream()
+                .map(color -> {
+                    String keyword = "Car " + car.trimYear() + " " + car.make() + " " + car.model() + " "
+                            + car.trimName() + " " + color;
+                    return fetchImageUrl(keyword)
+                            .onItem().transform(url -> Map.entry(color, url));
+                })
+                .toList();
+
+        return Uni.combine().all().unis(imageUnis)
+                .with(entries -> {
+                    Map<String, String> result = new HashMap<>();
+                    for (Object entry : entries) {
+                        Map.Entry<String, String> mapEntry = (Map.Entry<String, String>) entry;
+                        result.put(mapEntry.getKey(), mapEntry.getValue());
+                    }
+
+                    List<String> colorList = new ArrayList<>(result.keySet());
+                    List<String> urlList = new ArrayList<>(result.values());
+                    urlList.removeIf(String::isEmpty);
+
+                    if (!urlList.isEmpty()) {
+                        CarRepository.updateCarColorAndImageUrl(client, id, colorList, urlList);
+                    }
+
+                    return new GenericResponse<>(result, "Image fetched successfully");
+                });
     }
 
     /**
@@ -264,15 +309,18 @@ public class CarService {
     }
 
     /**
-     * Encodes the given keyword for use in a URL and constructs a Google Image search URL.
+     * Encodes the given keyword for use in a URL and constructs a Google Image
+     * search URL.
      *
      * @param keyword the keyword to be encoded and used in the search URL
-     * @return the constructed Google Image search URL with the encoded keyword, or null if encoding fails
+     * @return the constructed Google Image search URL with the encoded keyword, or
+     *         null if encoding fails
      */
     private String encodeKeyword(String keyword) {
         try {
             String encodedKeyword = java.net.URLEncoder.encode(keyword, "UTF-8");
-            String searchUrl = "https://www.google.com/search?q=" + encodedKeyword + "&tbs=isz:l&biw=1528&bih=738&dpr=1.25&tbm=isch&hl=en";
+            String searchUrl = "https://www.google.com/search?q=" + encodedKeyword
+                    + "&tbs=isz:l&biw=1528&bih=738&dpr=1.25&tbm=isch&hl=en";
             Log.info(searchUrl);
             return searchUrl;
         } catch (java.io.UnsupportedEncodingException e) {
@@ -284,8 +332,10 @@ public class CarService {
     /**
      * Fetches the content of the given search URL asynchronously.
      *
-     * @param searchUrl the URL to fetch content from. If null, a Uni containing a null item is returned.
-     * @return a Uni that emits the content of the URL as a String, or null if an error occurs.
+     * @param searchUrl the URL to fetch content from. If null, a Uni containing a
+     *                  null item is returned.
+     * @return a Uni that emits the content of the URL as a String, or null if an
+     *         error occurs.
      */
     private Uni<String> fetchSearchUrl(String searchUrl) {
         if (searchUrl == null) {
@@ -293,7 +343,9 @@ public class CarService {
         }
 
         String[] userAgents = {
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+                // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like
+                // Gecko) Chrome/58.0.3029.110 Safari/537.3",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0"
         };
         String userAgent = userAgents[new java.util.Random().nextInt(userAgents.length)];
 
@@ -306,7 +358,7 @@ public class CarService {
                                 .uri(new java.net.URI(searchUrl))
                                 .header("User-Agent", userAgent)
                                 .build(), java.net.http.HttpResponse.BodyHandlers.ofString())
-                        .thenApply(response -> response.body());
+                        .thenApply(java.net.http.HttpResponse::body);
             } catch (Exception e) {
                 Log.error("Error fetching search URL", e);
                 return java.util.concurrent.CompletableFuture.completedFuture(null);
@@ -318,7 +370,8 @@ public class CarService {
      * Fetches the URL of the first image from the given HTML response body.
      *
      * @param responseBody the HTML response body as a String
-     * @return a Uni containing the URL of the first image if found, or an empty string if no image is found or if the response body is null
+     * @return a Uni containing the URL of the first image if found, or an empty
+     *         string if no image is found or if the response body is null
      */
     private Uni<String> fetchImageFromUrl(String responseBody) {
         if (responseBody == null) {
@@ -330,6 +383,50 @@ public class CarService {
         if (firstImage != null) {
             Log.info(firstImage.attr("src"));
         }
+
+        String imageUrl = "";
+
+        if (firstImage == null && connectionString != null) {
+            String regex = "'data:image/jpeg;base64,([^']+)";
+
+            java.util.regex.Pattern pattern = java.util.regex.Pattern.compile(regex);
+            java.util.regex.Matcher matcher = pattern.matcher(responseBody);
+
+            while (matcher.find()) {
+                String base64 = matcher.group(1).replaceFirst("data:image/jpeg;base64,", "");
+                String cleanedBase64 = base64.replaceAll("\\\\x[0-9a-fA-F]{2}", "");
+                if (cleanedBase64.length() >= 5000) {
+                    byte[] bytes = java.util.Base64.getDecoder().decode(cleanedBase64);
+                    // try {
+                    // java.io.File file = java.io.File.createTempFile("image", ".jpg");
+                    // Log.info(file.toPath());
+                    // Log.info(file.toURI().toString());
+                    // java.nio.file.Files.write(file.toPath(), bytes);
+
+                    Mono<String> blobUrl = blobServiceAsyncClient
+                            .createBlobContainerIfNotExists("dev")
+                            .map(it -> it.getBlobAsyncClient("images/" + java.util.UUID.randomUUID() + ".jpg"))
+                            // .flatMap(it -> it.upload(BinaryData.fromBytes(bytes), true))
+                            .flatMap(blobAsyncClient -> blobAsyncClient.upload(BinaryData.fromBytes(bytes), true)
+                                    .map(blockBlobItem -> blobAsyncClient.getBlobUrl()));
+
+                    imageUrl = Uni.createFrom().completionStage(blobUrl.toFuture()).await().indefinitely();
+
+                    // } catch (java.io.IOException e) {
+                    // Log.error("Error writing image to file", e);
+                    // }
+                    // only fetch one image per color to saving cost on storage
+                    break;
+                }
+            }
+
+            Log.info("Azure Image Url: " + imageUrl);
+        }
+
+        if (!imageUrl.isEmpty()) {
+            return Uni.createFrom().item(imageUrl);
+        }
+
         return Uni.createFrom().item(firstImage != null ? firstImage.attr("src") : "");
     }
 
